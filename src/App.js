7e9491541aeaefe5ecdf00525a5130630bf1ac91
hoon2 <sanghoon.engineer@gmail.com>
2022-10-10 10:54:18 +0900

    컴포넌트 순수성 초기코드

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import NFT from "./components/NFT";
 function App() {
-  return <></>;
+  return (
+    <>
+      <NFT name="Cat" price={0.555} />
+      <NFT name="Cat" price={0.555} />
+      <NFT name="Cat" price={0.555} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/NFT.jsx
@@ -0,0 +1,16 @@
+function NFT({ name, price }) {
+  return (
+    <>
+      <div>
+        <p>Name</p>
+        <p>{name}</p>
+      </div>
+      <div>
+        <p>Price</p>
+        <p>{price} Either</p>
+      </div>
+    </>
+  );
+}
+
+export default NFT;
